be84a6c3dc91b92d5bc3f50d77568efd7883b478
hybras <24651269+hybras@users.noreply.github.com>
2020-08-17 15:45:22 -0400

    Prelimary setter for username property

--- src/vpn/util/settings.rs
+++ src/vpn/util/settings.rs
@@ -1,1 +1,35 @@
 use super::*;
+use anyhow::{Context, Result};
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Read, Write};
+
+/// Set the ProtonVPN Username
+fn set_username(user_config: &mut UserConfig) -> Result<String> {
+    let mut stdin = stdin();
+    let mut sin = stdin.lock();
+    let mut stdout = stdout();
+    let mut out = stdout.lock();
+    out.write_all(b"Enter your ProtonVPN OpenVPN username: ")?;
+    let mut username = String::new();
+    let old_username = user_config.username.clone();
+    sin.read_line(&mut user_config.username)?;
+    Ok(old_username)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+    use super::*;
+
+    #[test]
+    fn test_set_username() {
+        let mut user_config = UserConfig::with_user("default".into());
+        let old = set_username(&mut user_config);
+        match old {
+            Ok(old) => {
+                println!("{}", user_config.username);
+                assert_eq!(old, "default");
+            }
+            Err(_) => assert!(false, "Setting username failed"),
+        }
+    }
+}
