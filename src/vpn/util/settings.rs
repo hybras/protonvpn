529b0ad324c375a899bb98f811b8e00cf6f27177
hybras <24651269+hybras@users.noreply.github.com>
2020-08-19 19:56:02 -0400

    Move settings mutators into their own struct

--- src/vpn/util/settings.rs
+++ src/vpn/util/settings.rs
@@ -1,32 +1,51 @@
 use super::*;
 use anyhow::{Context, Result};
-use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Read, Write};
+use std::io::{
+    stdin, stdout, BufRead, BufReader, BufWriter, Read, Stdin, StdinLock, Stdout, StdoutLock, Write,
+};
 
-/// Set the ProtonVPN Username
-fn set_username(user_config: &mut UserConfig) -> Result<String> {
-    let mut stdin = stdin();
-    let mut sin = stdin.lock();
-    let mut stdout = stdout();
-    let mut out = stdout.lock();
-    out.write_all(b"Enter your ProtonVPN OpenVPN username: ")?;
-    let mut username = String::new();
-    let old_username = user_config.username.clone();
-    sin.read_line(&mut user_config.username)?;
-    Ok(old_username)
+#[derive(Default)]
+struct SettingsMutator {
+    user_config: UserConfig,
+}
+
+impl From<UserConfig> for SettingsMutator {
+    fn from(user_config: UserConfig) -> Self {
+        Self { user_config }
+    }
+}
+
+impl SettingsMutator {
+    /// Set the ProtonVPN Username
+    fn set_username(&mut self) -> Result<String> {
+        // Preamble for all set methods
+        // I don't understand lifetimes.
+        let stdin = stdin();
+        let mut sin = stdin.lock();
+        let stdout = stdout();
+        let mut out = stdout.lock();
+        // End preamble
+        out.write_all(b"Enter your ProtonVPN OpenVPN username: ")?;
+        let old_username = self.user_config.username.clone();
+        sin.read_line(&mut self.user_config.username)?;
+        Ok(old_username)
+    }
+ 
 }
 
 #[cfg(test)]
 mod tests {
-    use super::super::*;
     use super::*;
 
     #[test]
     fn test_set_username() {
-        let mut user_config = UserConfig::with_user("default".into());
-        let old = set_username(&mut user_config);
+        let mut settings = SettingsMutator {
+            user_config: UserConfig::with_user("default".into()),
+        };
+        let old = settings.set_username();
         match old {
             Ok(old) => {
-                println!("{}", user_config.username);
+                println!("{}", settings.user_config.username);
                 assert_eq!(old, "default");
             }
             Err(_) => assert!(false, "Setting username failed"),
