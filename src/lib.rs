5a74562015c9d110540f5368e9470b5b7e738330
hybras <24651269+hybras@users.noreply.github.com>
2020-12-31 18:31:52 -0500

    ♻️ move main func body to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,57 @@
+use crate::{
+    cli::{cli_hooks::*, CliOptions},
+    constants::APP_NAME,
+    vpn::util::Config,
+};
+use anyhow::{Context, Result};
+use confy::ConfyError;
+use std::io::{BufRead, Write};
+use CliOptions::*;
+
 pub mod cli;
-pub(crate) mod constants;
+pub mod constants;
 mod utils;
 pub mod vpn;
+
+pub fn main<R, W>(
+    opt: CliOptions,
+    config_res: Result<Config, ConfyError>,
+    mut r: &mut R,
+    mut w: &mut W,
+) -> Result<()>
+where
+    R: BufRead,
+    W: Write,
+{
+    if let Ok(mut config) = config_res {
+        match opt {
+            Init => {
+                initialize(&mut config.user, &mut r, &mut w)?;
+                confy::store(APP_NAME, config.user).context("Couldn't store your configuration")?;
+            }
+            Connect {
+                connection_option: _,
+                protocol: _,
+            } => {}
+            Reconnect => {}
+            Disconnect => {}
+            Status => {}
+            Configure => {
+                configure(&mut config.user, &mut r, &mut w)?;
+                confy::store(APP_NAME, config.user).context("Couldn't store your configuration")?;
+            }
+            Refresh => {}
+            Examples => {}
+        };
+    } else {
+        if let Init = opt {
+            initialize(&mut Default::default(), &mut r, &mut w)?;
+        } else {
+            writeln!(
+                &mut w,
+                "Unable to load your profile. Try running `protonvpn init` again."
+            )?;
+        }
+    }
+    Ok(())
+}
