67837befde079e577b0cc06193e8d630e64294dd
hybras <24651269+hybras@users.noreply.github.com>
2020-12-31 20:52:40 -0500

    🎨 run rustfmt

--- src/vpn/util.rs
+++ src/vpn/util.rs
@@ -11,118 +11,118 @@ pub mod settings;
 /// Holds current connection information and settings for the current (only) user
 #[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Config {
-    /// Actual config info
-    pub user: UserConfig,
-    /// Current connection information
-    pub connection_info: Option<ConnectionInfo>,
-    /// Random extra info
-    pub metadata: MetaData,
+	/// Actual config info
+	pub user: UserConfig,
+	/// Current connection information
+	pub connection_info: Option<ConnectionInfo>,
+	/// Random extra info
+	pub metadata: MetaData,
 }
 
 /// Hold's all user settings. See the docs on each field to learn more.
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct UserConfig {
-    pub(crate) username: Option<String>,
-    pub(crate) password: Option<String>,
-    pub(crate) tier: PlanTier,
-    pub(crate) protocol: ConnectionProtocol,
-    /// A recommended security setting that enables using Proton VPN's dns servers, or your own. In other words, don't use the dns servers from your operating system / internet service provider
-    pub(crate) dns_leak_protection: bool,
-    /// This setting is only referenced if the dns_leak_protection is enabled
-    pub(crate) custom_dns: Vec<Ipv4Addr>,
-    pub(crate) check_update_interval: u8,
-    pub(crate) killswitch: u8,
-    pub(crate) split_tunnel: bool,
-    // Remove this field. It can't change. Its always the default (see impl Default)
-    pub(crate) api_domain: Url,
+	pub(crate) username: Option<String>,
+	pub(crate) password: Option<String>,
+	pub(crate) tier: PlanTier,
+	pub(crate) protocol: ConnectionProtocol,
+	/// A recommended security setting that enables using Proton VPN's dns servers, or your own. In other words, don't use the dns servers from your operating system / internet service provider
+	pub(crate) dns_leak_protection: bool,
+	/// This setting is only referenced if the dns_leak_protection is enabled
+	pub(crate) custom_dns: Vec<Ipv4Addr>,
+	pub(crate) check_update_interval: u8,
+	pub(crate) killswitch: u8,
+	pub(crate) split_tunnel: bool,
+	// Remove this field. It can't change. Its always the default (see impl Default)
+	pub(crate) api_domain: Url,
 }
 
 /// Creates unusable initial state. Must set the username  and password fields (is initially None)
 impl Default for UserConfig {
-    fn default() -> Self {
-        Self {
-            username: None,
-            password: None,
-            tier: PlanTier::Free,
-            protocol: ConnectionProtocol::UDP,
-            dns_leak_protection: true,
-            custom_dns: Vec::with_capacity(3),
-            check_update_interval: 3,
-            killswitch: 0,
-            split_tunnel: false,
-            api_domain: Url::parse("https://api.protonvpn.ch")
-                .context("Failed to parse protonvpn api url")
-                .unwrap(),
-        }
-    }
+	fn default() -> Self {
+		Self {
+			username: None,
+			password: None,
+			tier: PlanTier::Free,
+			protocol: ConnectionProtocol::UDP,
+			dns_leak_protection: true,
+			custom_dns: Vec::with_capacity(3),
+			check_update_interval: 3,
+			killswitch: 0,
+			split_tunnel: false,
+			api_domain: Url::parse("https://api.protonvpn.ch")
+				.context("Failed to parse protonvpn api url")
+				.unwrap(),
+		}
+	}
 }
 #[derive(
-    Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Copy, Clone, EnumIter, Display,
+	Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Copy, Clone, EnumIter, Display,
 )]
 pub(crate) enum PlanTier {
-    Free,
-    Basic,
-    Plus,
-    Visionary,
+	Free,
+	Basic,
+	Plus,
+	Visionary,
 }
 
 impl From<u8> for PlanTier {
-    fn from(u: u8) -> Self {
-        use PlanTier::*;
+	fn from(u: u8) -> Self {
+		use PlanTier::*;
 
-        match u {
-            0 => Free,
-            1 => Basic,
-            2 => Plus,
-            3 => Visionary,
-            _ => panic!("u8 was too big to convert to PlanTier"),
-        }
-    }
+		match u {
+			0 => Free,
+			1 => Basic,
+			2 => Plus,
+			3 => Visionary,
+			_ => panic!("u8 was too big to convert to PlanTier"),
+		}
+	}
 }
 
 /// Order here is used to indicate default option: UDP
 #[derive(Debug, Serialize, Deserialize, PartialEq, Copy, Clone, EnumIter, Display)]
 pub enum ConnectionProtocol {
-    UDP,
-    TCP,
+	UDP,
+	TCP,
 }
 
 impl Default for ConnectionProtocol {
-    fn default() -> Self {
-        Self::UDP
-    }
+	fn default() -> Self {
+		Self::UDP
+	}
 }
 
 impl FromStr for ConnectionProtocol {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_ascii_lowercase().as_str() {
-            "udp" => Ok(Self::UDP),
-            "tcp" => Ok(Self::TCP),
-            _ => Err("String must be udp or tcp".into()),
-        }
-    }
+	type Err = String;
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		match s.to_ascii_lowercase().as_str() {
+			"udp" => Ok(Self::UDP),
+			"tcp" => Ok(Self::TCP),
+			_ => Err("String must be udp or tcp".into()),
+		}
+	}
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct MetaData {
-    pub(crate) resolvconf_hash: Option<String>,
-    pub(crate) last_api_pull: DateTime<Utc>,
+	pub(crate) resolvconf_hash: Option<String>,
+	pub(crate) last_api_pull: DateTime<Utc>,
 }
 
 impl Default for MetaData {
-    fn default() -> Self {
-        Self {
-            resolvconf_hash: None,
-            last_api_pull: Utc.timestamp_millis(0),
-        }
-    }
+	fn default() -> Self {
+		Self {
+			resolvconf_hash: None,
+			last_api_pull: Utc.timestamp_millis(0),
+		}
+	}
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct ConnectionInfo {
-    pub(crate) server: String,
-    pub(crate) protocol: ConnectionProtocol,
-    pub(crate) dns_server: String,
-    pub(crate) connected_time: String,
+	pub(crate) server: String,
+	pub(crate) protocol: ConnectionProtocol,
+	pub(crate) dns_server: String,
+	pub(crate) connected_time: String,
 }
--- src/vpn/util/settings.rs
+++ src/vpn/util/settings.rs
@@ -10,133 +10,133 @@ use strum::IntoEnumIterator;
 ///
 /// In the future, this struct should store stdin/stdout handles for buffering, and write settings upon Drop.
 pub(crate) struct Settings<'a, S, R: BufRead, W: Write> {
-    settings: S,
-    stdout: &'a mut W,
-    stdin: &'a mut R,
+	settings: S,
+	stdout: &'a mut W,
+	stdin: &'a mut R,
 }
 
 impl<'a, S, R: BufRead, W: Write> Settings<'a, S, R, W> {
-    pub fn new(settings: S, stdout: &'a mut W, stdin: &'a mut R) -> Self {
-        Self {
-            settings,
-            stdout,
-            stdin,
-        }
-    }
-    fn set_enum_field<T, N>(
-        &mut self,
-        name: N,
-        getter: impl Fn(&S) -> T,
-        setter: impl Fn(&mut S, T) -> (),
-    ) -> Result<T>
-    where
-        T: Display + Copy + IntoEnumIterator,
-        N: AsRef<str>,
-    {
-        writeln!(self.stdout, "{}: ", name.as_ref())?;
-        let options: Vec<T> = T::iter().collect();
-        for (idx, option) in options.iter().enumerate() {
-            writeln!(&mut self.stdout, "\t{}) {}", idx, option.to_string())?;
-        }
-        let old_value = getter(&mut self.settings);
-        let mut new_value = String::new();
-        let new_value = loop {
-            write!(self.stdout, "Enter {}: ", name.as_ref())?;
-            self.stdout.flush()?;
+	pub fn new(settings: S, stdout: &'a mut W, stdin: &'a mut R) -> Self {
+		Self {
+			settings,
+			stdout,
+			stdin,
+		}
+	}
+	fn set_enum_field<T, N>(
+		&mut self,
+		name: N,
+		getter: impl Fn(&S) -> T,
+		setter: impl Fn(&mut S, T) -> (),
+	) -> Result<T>
+	where
+		T: Display + Copy + IntoEnumIterator,
+		N: AsRef<str>,
+	{
+		writeln!(self.stdout, "{}: ", name.as_ref())?;
+		let options: Vec<T> = T::iter().collect();
+		for (idx, option) in options.iter().enumerate() {
+			writeln!(&mut self.stdout, "\t{}) {}", idx, option.to_string())?;
+		}
+		let old_value = getter(&mut self.settings);
+		let mut new_value = String::new();
+		let new_value = loop {
+			write!(self.stdout, "Enter {}: ", name.as_ref())?;
+			self.stdout.flush()?;
 
-            self.stdin.read_line(&mut new_value)?;
-            let possible_value: usize = new_value.trim().parse()?;
-            if (0..options.len()).contains(&(possible_value as usize)) {
-                break possible_value;
-            } else {
-                writeln!(&mut self.stdout, "Enter a 𝑣𝑎𝑙𝑖𝑑 number")?;
-                continue;
-            }
-        };
-        setter(&mut self.settings, options[new_value].clone());
-        Ok(old_value)
-    }
+			self.stdin.read_line(&mut new_value)?;
+			let possible_value: usize = new_value.trim().parse()?;
+			if (0..options.len()).contains(&(possible_value as usize)) {
+				break possible_value;
+			} else {
+				writeln!(&mut self.stdout, "Enter a 𝑣𝑎𝑙𝑖𝑑 number")?;
+				continue;
+			}
+		};
+		setter(&mut self.settings, options[new_value].clone());
+		Ok(old_value)
+	}
 
-    fn set_value_field<T, N>(
-        &mut self,
-        name: N,
-        getter: impl Fn(&S) -> T,
-        setter: impl Fn(&mut S, T) -> (),
-    ) -> Result<T>
-    where
-        T: Display + FromStr,
-        N: AsRef<str>,
-        <T as FromStr>::Err: std::marker::Sync + std::error::Error + std::marker::Send + 'static,
-    {
-        write!(self.stdout, "Enter your {}: ", name.as_ref())?;
-        self.stdout.flush()?;
-        let old_value = getter(&self.settings);
-        let mut new_value = String::new();
-        self.stdin.read_line(&mut new_value)?;
-        let new = new_value.trim().parse::<T>()?;
-        setter(&mut self.settings, new);
-        Ok(old_value)
-    }
+	fn set_value_field<T, N>(
+		&mut self,
+		name: N,
+		getter: impl Fn(&S) -> T,
+		setter: impl Fn(&mut S, T) -> (),
+	) -> Result<T>
+	where
+		T: Display + FromStr,
+		N: AsRef<str>,
+		<T as FromStr>::Err: std::marker::Sync + std::error::Error + std::marker::Send + 'static,
+	{
+		write!(self.stdout, "Enter your {}: ", name.as_ref())?;
+		self.stdout.flush()?;
+		let old_value = getter(&self.settings);
+		let mut new_value = String::new();
+		self.stdin.read_line(&mut new_value)?;
+		let new = new_value.trim().parse::<T>()?;
+		setter(&mut self.settings, new);
+		Ok(old_value)
+	}
 
-    pub(crate) fn inner(self) -> S {
-        self.settings
-    }
+	pub(crate) fn inner(self) -> S {
+		self.settings
+	}
 }
 
 /// Adds named setters for UserConfig properties
 impl<'a, R: BufRead, W: Write> Settings<'a, UserConfig, R, W> {
-    /// Set the ProtonVPN Username
-    pub(crate) fn set_username(&mut self) -> Result<String> {
-        self.set_value_field(
-            "username",
-            |u| u.username.clone().unwrap_or("".into()),
-            |u, t| u.username = Some(t),
-        )
-    }
+	/// Set the ProtonVPN Username
+	pub(crate) fn set_username(&mut self) -> Result<String> {
+		self.set_value_field(
+			"username",
+			|u| u.username.clone().unwrap_or("".into()),
+			|u, t| u.username = Some(t),
+		)
+	}
 
-    /// Set the users ProtonVPN Plan.
-    pub(crate) fn set_tier(&mut self) -> Result<PlanTier> {
-        self.set_enum_field("Plan Tier", |t| t.tier, |u, t| u.tier = t)
-    }
+	/// Set the users ProtonVPN Plan.
+	pub(crate) fn set_tier(&mut self) -> Result<PlanTier> {
+		self.set_enum_field("Plan Tier", |t| t.tier, |u, t| u.tier = t)
+	}
 
-    pub(crate) fn set_protocol(&mut self) -> Result<ConnectionProtocol> {
-        self.set_enum_field("Connection Protocol", |u| u.protocol, |u, t| u.protocol = t)
-    }
+	pub(crate) fn set_protocol(&mut self) -> Result<ConnectionProtocol> {
+		self.set_enum_field("Connection Protocol", |u| u.protocol, |u, t| u.protocol = t)
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_set_username() {
-        let mut input = "hello\n".as_bytes();
-        let mut output = vec![];
-        let mut settings = Settings::new(UserConfig::default(), &mut output, &mut input);
-        let old = settings.set_username();
-        let user_config = settings.inner();
-        match old {
-            Ok(old) => {
-                assert_eq!(user_config.username, Some("hello".into()));
-                assert_eq!(old, "");
-            }
-            Err(_) => assert!(false, "Setting username failed"),
-        }
-    }
+	#[test]
+	fn test_set_username() {
+		let mut input = "hello\n".as_bytes();
+		let mut output = vec![];
+		let mut settings = Settings::new(UserConfig::default(), &mut output, &mut input);
+		let old = settings.set_username();
+		let user_config = settings.inner();
+		match old {
+			Ok(old) => {
+				assert_eq!(user_config.username, Some("hello".into()));
+				assert_eq!(old, "");
+			}
+			Err(_) => assert!(false, "Setting username failed"),
+		}
+	}
 
-    #[test]
-    fn test_set_tier() {
-        let mut input = "2\n".as_bytes();
-        let mut output = vec![];
-        let mut settings = Settings::new(UserConfig::default(), &mut output, &mut input);
-        let old = settings.set_tier();
-        let user_config = settings.inner();
-        match old {
-            Ok(old) => {
-                assert_eq!(user_config.tier, PlanTier::Plus);
-                assert_eq!(old, PlanTier::Free);
-            }
-            Err(_) => assert!(false, "Setting Tier failed"),
-        }
-    }
+	#[test]
+	fn test_set_tier() {
+		let mut input = "2\n".as_bytes();
+		let mut output = vec![];
+		let mut settings = Settings::new(UserConfig::default(), &mut output, &mut input);
+		let old = settings.set_tier();
+		let user_config = settings.inner();
+		match old {
+			Ok(old) => {
+				assert_eq!(user_config.tier, PlanTier::Plus);
+				assert_eq!(old, PlanTier::Free);
+			}
+			Err(_) => assert!(false, "Setting Tier failed"),
+		}
+	}
 }
