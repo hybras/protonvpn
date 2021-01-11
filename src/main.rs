541a7577c41e2a0dd3cced2e33e7c5bc0217c062
hybras <24651269+hybras@users.noreply.github.com>
2021-01-10 22:22:24 -0500

    ⚡️ Use paw in main function

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,16 @@ use anyhow::Result;
 use confy::load;
 use protonvpn::{cli::CliOptions, constants::APP_NAME, main as main_cli, vpn::util::Config};
 use std::io::{stdin, stdout};
-use structopt::StructOpt;
 
-fn main() -> Result<()> {
+#[paw::main]
+fn main(args: CliOptions) -> Result<()> {
 	// Get stdio handles. These are passed through the entire program
 	let stdin = stdin();
 	let mut in_lock = stdin.lock();
 	let stdout = stdout();
 	let mut out_lock = stdout.lock();
 
-	let opt = CliOptions::from_args();
 	let config = load::<Config>(APP_NAME);
 
-	main_cli(opt, config, &mut in_lock, &mut out_lock)
+	main_cli(args, config, &mut in_lock, &mut out_lock)
 }
